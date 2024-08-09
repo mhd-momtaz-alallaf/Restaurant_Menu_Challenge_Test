17ff15528b70174e6ef79aca4b8fbc4379d449dd
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-08-09 18:21:28 +0300

    Modifying the Discounts migration to have the morph relation instead of foreignIds

--- database/migrations/2024_08_09_120618_create_discounts_table.php
+++ database/migrations/2024_08_09_120618_create_discounts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->decimal('discount_value', 5, 2);
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->morphs('discountable');
             $table->timestamps();
         });
     }
