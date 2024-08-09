c5d830350becf805a15b567c033d6212d9121592
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-08-09 15:37:16 +0300

    Implementing the CategoryRequest and the store method of the CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function store(CategoryRequest $request)
+    {
+        $parent = Category::find($request['parent_id']);
+        $level = $parent ? $parent->level + 1 : 1;
+
+        Category::create([
+            'name' => $request['name'],
+            'parent_id' => $request['parent_id'],
+            'level' => $level,
+        ]);
+
+        return response()->json([
+            'message' => 'Category created successfully'
+        ], 201);
+    }
 }
