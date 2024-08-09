2191f2e7cc6158b7d70e5b40309591afcc505275
mhd-momtaz-alallaf <parental.co122@gmail.com>
2024-08-09 15:12:14 +0300

    Implementing the Category Model and Migration with the parent and children self relations in the model

--- database/migrations/2024_08_09_120431_create_categories_table.php
+++ database/migrations/2024_08_09_120431_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
+            $table->unsignedInteger('level')->default(1);
             $table->timestamps();
         });
     }
